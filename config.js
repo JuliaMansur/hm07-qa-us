b9df5302cf8d0cccbd996b7b60d6823600195d76
Julia Mansur <julia.mansur.rodrigues@gmail.com>
2025-05-09 15:29:20 +0300

    adding GET,PUT,DELETE and POST test to Urban Grocers

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-5ab16a1a-1eb6-4b2c-ad32-72b3732deb48.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-327d8c04-340c-4dd1-938d-215849cb6e53.containerhub.tripleten-services.com"
 }
